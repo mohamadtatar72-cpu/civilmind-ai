fbddd667c69f49a9f33c207fd0f27649524e8a98
mohamadtatar72-cpu <mohamad.tatar72@gmail.com>
2026-07-30 22:55:25 +0330

    Sprint 1C: add admin users route

---
+++ app/admin/users/page.tsx
@@ -0,0 +1,10 @@
+import AppShell from "@/components/layout/app-shell";
+import AdminUsers from "@/components/admin/admin-users";
+
+export default function AdminUsersPage() {
+  return (
+    <AppShell>
+      <AdminUsers />
+    </AppShell>
+  );
+}
