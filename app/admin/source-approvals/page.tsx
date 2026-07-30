94a34977e5cad13e167c9a4a12b343271b7993d0
mohamadtatar72-cpu <mohamad.tatar72@gmail.com>
2026-07-30 22:55:43 +0330

    Sprint 1C: add official source approvals route

---
+++ app/admin/source-approvals/page.tsx
@@ -0,0 +1,10 @@
+import AppShell from "@/components/layout/app-shell";
+import SourceApprovalsDashboard from "@/components/admin/source-approvals-dashboard";
+
+export default function SourceApprovalsPage() {
+  return (
+    <AppShell>
+      <SourceApprovalsDashboard />
+    </AppShell>
+  );
+}
