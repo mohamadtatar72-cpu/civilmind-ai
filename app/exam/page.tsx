063125969cc9b646153c26d7c60224f6f99f0133
mohamadtatar72-cpu <mohamad.tatar72@gmail.com>
2026-07-31 07:22:10 +0000

    fix(nav): separate exam and analytics active routes

---
+++ app/exam/page.tsx
@@ -0,0 +1,10 @@
+import AppShell from "@/components/layout/app-shell";
+import ExamCenter from "@/components/exam/exam-center";
+
+export default function AnalyticsPage() {
+  return (
+    <AppShell>
+      <ExamCenter />
+    </AppShell>
+  );
+}
